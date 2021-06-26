9d79de8e6ed50617e708c8b11c2666b8be6860d4
AnchanaK24 <74914989+AnchanaK24@users.noreply.github.com>
2021-06-26 14:02:37 +0530

    Update gmail_app.py

--- gmail_app.py
+++ gmail_app.py
@@ -1,13 +1,30 @@
 from __future__ import print_function
 import os.path
-import pprint
+import sqlalchemy as db
+from sqlalchemy import Table, Column, Integer, String
+from requests import Session
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 
+engine = db.create_engine('sqlite:///gmail.db', echo=True)
+meta = db.MetaData()
+
+
+mail = Table(
+   'mail', meta,
+   Column('id', Integer, primary_key=True),
+   Column('description', String),
+   Column('thID', String),
+)
+session = Session()
+meta.create_all(engine)
+
+
+
 def get_gmail_service():
-    SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+    SCOPES = ['https://www.googleapis.com/auth/gmail.modify']
     creds = None
     if os.path.exists('token.json'):
         creds = Credentials.from_authorized_user_file('token.json', SCOPES)
@@ -31,14 +48,19 @@ def get_email_list():
 
 def get_email_content(message_id):
     service=get_gmail_service()
-    data=service.users().messages().get(userId='me',id=message_id).execute()
+    results = service.users().messages().get(userId='me', id=message_id).execute()
+    data = {'snippet': results['snippet'],'threadId': results['threadId']}
     return data
+
+def store():
+    engine = db.create_engine('sqlite:///gmail.db', echo=True)
+    conn = engine.connect()
+    result = get_email_content('17a3e5114762c774')
+    conn.execute('INSERT INTO mail(description,thId) VALUES(:description,:thId)',
+                 (result['snippet']), result['threadId'])
+    print("entered successfully")
+    conn.close()
 
 
 if __name__ == '__main__':
-    #messages=get_email_list()
-    #for message in messages:
-    #    print(message['id'])
-
-    pprint.pprint(get_email_content('17a3e5114762c774'))
-    
+    store()
