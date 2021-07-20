ec0b0b60f3998e61d597c9aca635e54d006058e0
AnchanaK24 <74914989+AnchanaK24@users.noreply.github.com>
2021-07-20 10:24:00 +0530

    Update store.py

--- models/store.py
+++ models/store.py
@@ -6,12 +6,12 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 
-from service.gmail_api import get_email_content
+from models.gmail import get_mail
 
-SCOPES = 'https://www.googleapis.com/auth/gmail.modify'
+SCOPES = ['https://www.googleapis.com/auth/gmail.modify']
 
 
-def get_gmail_service():
+def get_service():
     creds = None
     if os.path.exists('../token.json'):
         creds = Credentials.from_authorized_user_file('../token.json', SCOPES)
@@ -30,16 +30,47 @@ def get_gmail_service():
 
 
 def store():
-    engine = db.create_engine('sqlite:///gmail.db', echo=True)
+    engine = db.create_engine('sqlite:///user.db', echo=True)
     conn = engine.connect()
-    result= get_email_content('17a3e5114762c774')
-    conn.execute('INSERT INTO mail(mail_from,mail_to,mail_subject,mail_date) VALUES (:mail_from,:mail_to,:mail_subject,:mail_date)',
-                 result['from'], result['to'], result['subject'], result['date'])
-    print('logged successfully')
+    result = get_mail(''17a3e5114762c774'')
+    conn.execute('INSERT INTO mail(mail_from,mail_to,mail_subject,mail_date)  VALUES(:mail_from,:mail_to,:mail_subject,:mail_date)',
+                 results['from'], results['to'], results['subject'], results['date'])
+    print("entered successfully")
     conn.close()
 
+ 
+def mark_as_unread():
+    rules = json.load(open('rules.json'))
+    for rule in rules["rule1"]["fields"]:
+        print(rule['name'], rule['value'])
+        service = get_service()
+        service.users().messages().modify(userId='me', id=''17a3e5114762c774'',
+                                          body={'addLabelIds': ['UNREAD']}).execute()
+
+        
+        
+ def mark_as_read():
+    rules = json.load(open('rules.json'))
+    for rule in rules["rule1"]["fields"]:
+        print(rule['name'], rule['value'])
+        service = get_service()
+        service.users().messages().modify(userId='me', id=''17a3e5114762c774'',
+                                          body={'removeLabelIds': ['UNREAD']}).execute()
 
 
+def archive_mail():
+    rules = json.load(open('rules.json'))
+    for rule in rules["rule1"]["fields"]:
+        print(rule['name'], rule['value'])
+        service = get_service()
+        service.users().messages().modify(userId='me', id=''17a3e5114762c774'',
+                                          body={'removeLabelIds': ['INBOX']}).execute()
 
-if __name__ == '__main__':
-    store()
+
+    
+    
+    if __name__ == '__main__':
+    #store()
+    #mark_as_unread()
+    #mark_as_read()
+    #archive_mail()
